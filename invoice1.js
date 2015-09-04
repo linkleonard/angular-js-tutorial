dbdb6b8ff63d86731974fb33484a3c33df1e25ac
Leonard Law <leon.wlaw@gmail.com>
2015-09-03 22:58:47 -0400

    Move finance related functions to a separate service

--- invoice1.js
+++ invoice1.js
@@ -1,24 +1,15 @@
-angular.module('invoice1', [])
-    .controller('InvoiceController', function() {
+angular.module('invoice1', ['finance2'])
+    .controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
         this.qty = 1;
         this.cost = 2;
         this.inCurr = 'EUR';
-        this.currencies = ['USD', 'EUR', 'CNY'];
-        this.usdToForeignRates = {
-            USD: 1,
-            EUR: 0.74,
-            CNY: 6.09,
-        };
+        this.currencies = currencyConverter.currencies;
 
         this.total = function total(outCurr) {
-            return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
-        };
-
-        this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
-            return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+            return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
         };
 
         this.pay = function pay() {
             window.alert('Thanks!');
         };
-    });
+    }]);
